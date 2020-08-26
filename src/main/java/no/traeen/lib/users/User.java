152496f10fad10e3260308b12b05dcfb11a35ba2
freshfish70 <christoffer.andersen@live.com>
2020-08-26 11:55:14 +0200

    Made user an entity

--- src/main/java/no/traeen/lib/users/User.java
+++ src/main/java/no/traeen/lib/users/User.java
@@ -1,20 +1,43 @@
 package no.traeen.lib.users;
 
+import java.math.BigInteger;
 import java.security.InvalidParameterException;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /* 
 Represents a User in the system.
 A user has a name, password.
  */
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private BigInteger userid;
+
+	@Column(nullable = false)
 	private String name;
 
+	@Column(nullable = false)
 	private String password;
+
+	public User() {
+	}
 
 	public User(String name, String password) {
 		this.setName(name);
 		this.setPassword(password);
+	}
+
+	public BigInteger getUserid() {
+		return this.userid;
 	}
 
 	public String getName() {
