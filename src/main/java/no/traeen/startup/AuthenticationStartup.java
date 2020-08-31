21562eb0369f3335b0e7717fe520e839ae2b4a4b
freshfish70 <christoffer.andersen@live.com>
2020-08-31 13:27:17 +0200

    Add authentication startup script

---
+++ src/main/java/no/traeen/startup/AuthenticationStartup.java
@@ -0,0 +1,36 @@
+package no.traeen.startup;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import no.traeen.lib.users.Group;
+
+/**
+ * Startup actions for authentication that are executed on server start.
+ */
+@Singleton
+@Startup
+public class AuthenticationStartup {
+	@PersistenceContext
+	EntityManager em;
+
+	@PostConstruct
+	public void init() {
+		persistUsergroups();
+	}
+
+	/**
+	 * Makes sure that our user grous are added to the database.
+	 */
+	public void persistUsergroups() {
+		long groups = (long) em.createQuery("SELECT count(g.name) from Group g").getSingleResult();
+		if (groups != Group.GROUPS.length) {
+			for (String group : Group.GROUPS) {
+				em.merge(new Group(group));
+			}
+		}
+	}
+}
