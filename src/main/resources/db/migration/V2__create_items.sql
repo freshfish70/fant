06ad6204e14d4d59b77d92d8a92a1c70c2673b4c
freshfish70 <christoffer.andersen@live.com>
2020-09-01 13:34:55 +0200

    Add create items database migration

---
+++ src/main/resources/db/migration/V2__create_items.sql
@@ -0,0 +1,44 @@
+
+
+create table items
+(
+	id serial not null
+		constraint items_pk
+			primary key,
+	name varchar,
+	description varchar,
+	price real default 0,
+	image integer,
+	sold boolean default false,
+	created date,
+	updated date,
+	seller integer
+		constraint items_users_id_fk
+			references users,
+	buyer integer
+		constraint items_users_id_fk_2
+			references users
+);
+
+alter table items owner to postgres;
+
+create unique index items_id_uindex
+	on items (id);
+
+create table images
+(
+	id serial not null
+		constraint images_pk
+			primary key,
+	name varchar,
+	"mimeType" varchar,
+	size real,
+	owner integer
+		constraint images_items_id_fk
+			references items
+);
+
+alter table images owner to postgres;
+
+create unique index images_id_uindex
+	on images (id);
