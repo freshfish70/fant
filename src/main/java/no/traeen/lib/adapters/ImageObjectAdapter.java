172159a9b40310f23cefd28c32b97b77d6791a56
freshfish70 <christoffer.andersen@live.com>
2020-09-04 12:34:30 +0200

    Moved image adapter

--- src/main/java/no/traeen/lib/adapters/ImageObjectAdapter.java
+++ src/main/java/no/traeen/lib/adapters/ImageObjectAdapter.java
@@ -1,4 +1,4 @@
-package no.traeen.lib;
+package no.traeen.lib.adapters;
 
 import no.traeen.lib.resource.Image;
 
@@ -9,14 +9,29 @@ import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
 import javax.json.bind.adapter.JsonbAdapter;
 
+/**
+ * Adapter to perform conversion on Image objects to/from JSON.
+ */
 public class ImageObjectAdapter implements JsonbAdapter<Set<Image>, JsonArray> {
+
+	/**
+	 * Extracts the ID of all images in a set to a JSON array containing all the
+	 * Id´s
+	 */
 	@Override
 	public JsonArray adaptToJson(Set<Image> mos) throws Exception {
 		JsonArrayBuilder result = Json.createArrayBuilder();
+		System.out.println(mos);
+		if (mos == null) {
+			return result.build();
+		}
 		mos.forEach(mo -> result.add(mo.getId()));
 		return result.build();
 	}
 
+	/**
+	 * Not implemented
+	 */
 	@Override
 	public Set<Image> adaptFromJson(JsonArray mediaid) throws Exception {
 		return null;
