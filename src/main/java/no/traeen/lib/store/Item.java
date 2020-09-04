ee966ba0d39608d3f6ae5d29d54b1488e6f03d09
freshfish70 <christoffer.andersen@live.com>
2020-09-04 12:35:07 +0200

    Fix import of adapter

--- src/main/java/no/traeen/lib/store/Item.java
+++ src/main/java/no/traeen/lib/store/Item.java
@@ -9,23 +9,20 @@ import java.util.Set;
 
 import javax.json.bind.annotation.JsonbTypeAdapter;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
-import javax.validation.constraints.Null;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-import no.traeen.lib.ImageObjectAdapter;
+import no.traeen.lib.adapters.ImageObjectAdapter;
 import no.traeen.lib.resource.Image;
 import no.traeen.lib.users.User;
 
