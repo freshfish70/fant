4ccc21b9fbcfece79788b498b9795d04003e5a73
freshfish70 <christoffer.andersen@live.com>
2020-09-08 10:39:03 +0200

    Edit run application only

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-docker run --network=fant_backend -d -p 9080:9080 -p 9443:9443 --name fant -v $(pwd)/target:/config/dropins no.traeen/fant
+docker container rm -f fant 
+docker network create fant_backend
+docker run --network=fant_backend -p 9080:9080 -p 9443:9443 --name fant -v $(pwd)/target:/config/dropins no.traeen/fant
