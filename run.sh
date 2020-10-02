c22e8486ceb3652d0f967379e5403254d10ec312
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-02 18:31:44 +0200

    Modify startup files

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 docker container rm -f fant 
-docker network create fant_backend
-docker run --network=fant_backend -p 9080:9080 -p 9443:9443 --name fant -v $(pwd)/target:/config/dropins no.traeen/fant
+docker run --network=fant_backend -p 9080:9080 -p 9443:9443 --user 1000 --name fant -v $(pwd)/target:/config/dropins -v $(pwd)/server:/opt/ol/wlp/output/defaultServer/ no.traeen/fant 
