61f8dc7295273e6fd0b45d4a181f48ea937db04b
Christoffer A Traeen <christoffer.andersen@live.com>
2020-10-15 00:27:35 +0200

    Fixes

--- src/main/java/no/traeen/startup/AuthenticationStartup.java
+++ src/main/java/no/traeen/startup/AuthenticationStartup.java
@@ -1,37 +1,43 @@
 package no.traeen.startup;
 
+import no.traeen.lib.users.Group;
+
 import javax.annotation.PostConstruct;
+import javax.ejb.DependsOn;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
-import no.traeen.lib.users.Group;
 
 /**
  * Startup actions for authentication that are executed on server start.
  */
 @Singleton
 @Startup
+@DependsOn("FlywayUpdater")
 public class AuthenticationStartup {
-	@PersistenceContext
-	EntityManager em;
+    @PersistenceContext
+    EntityManager em;
 
-	@PostConstruct
-	public void init() {
-		persistUsergroups();
-	}
+    @PostConstruct
+    public void init() {
+        persistUsergroups();
+    }
 
-	/**
-	 * Makes sure that our user grous are added to the database.
-	 */
-	public void persistUsergroups() {
-		// Thread.sleep(5000);
-		long groups = (long) em.createQuery("SELECT count(g.name) from Group g").getSingleResult();
-		if (groups != Group.GROUPS.length) {
-			for (String group : Group.GROUPS) {
-				em.merge(new Group(group));
-			}
-		}
-	}
+    /**
+     * Makes sure that our user grous are added to the database.
+     */
+    public void persistUsergroups() {
+        // Thread.sleep(5000);
+        try {
+            long groups = (long) em.createQuery("SELECT count(g.name) from Group g").getSingleResult();
+            if (groups != Group.GROUPS.length) {
+                for (String group : Group.GROUPS) {
+                    em.merge(new Group(group));
+                }
+            }
+        } catch (Exception e) {
+        }
+
+    }
 }
